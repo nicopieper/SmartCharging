5f13fe7c85e0953e8ccc2882c96f28c14f5d7bdf
Nico Pieper <nicopieper@gmail.com>
2020-12-08 12:59:39 +0100

    Include first raw version of LiveAlgo. Only ResPo controlling supported

--- Simulation.m
+++ Simulation.m
@@ -2,7 +2,7 @@
 tic
 %Why does this produce an error?
 %Compare for 2 Users with and without parallel
-NumUsers=10; % size(Users,1)-1
+NumUsers=20; % size(Users,1)-1
 SmartCharging=true;
 UseParallel=false;
 %UseParallel=true;
