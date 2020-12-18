17a7b8c184ceeec4991ea797b271ecf1baef7ea0
Nico Pieper <nicopieper@gmail.com>
2020-12-18 12:27:01 +0100

    Economic optimization add MaxPossibleSoC

--- Algorithm/CalcDynOptVars.m
+++ Algorithm/CalcDynOptVars.m
@@ -73,5 +73,5 @@ end
 PowerTS=repelem(MaxPower/4,ControlPeriodsIt,NumCostCats,1);
 PowerTS(:,2,:)=min([PowerTS(:,2,:), PVPower(end-ControlPeriodsIt+1:end,:,:)/4], [], 2);
 ConsPowerTSb=PowerTS;
-MinEnergyRequiredTS=min(min([MinEnergyRequiredTS, MaxEnergyChargableSoCTS, cumsum(SumPower,1)], [], 2), MaxEnergyChargableDeadlockCP);
+MinEnergyRequiredTS=min(min([MinEnergyRequiredTS, MaxEnergyChargableSoCTS], [], 2), MaxEnergyChargableDeadlockCP);
 
