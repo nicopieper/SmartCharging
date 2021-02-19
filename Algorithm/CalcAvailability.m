58b92d109227f882933993ea000763f3eb768a4a
nicopieper <nicopieper@gmail.com>
2021-02-19 17:01:16 +0100

    Prepare for final Sim

--- Algorithm/CalcAvailability.m
+++ Algorithm/CalcAvailability.m
@@ -15,16 +15,18 @@ for k=UserNum
 end
 
 
-Logbooks=zeros(ControlPeriodsIt,7,NumUsers);
+Logbooks1=zeros(ControlPeriodsIt,1,NumUsers);
+Logbooks2=zeros(ControlPeriodsIt,1,NumUsers);
 VarCounter=0;
 for k=UserNum
     VarCounter=VarCounter+1;
-    Logbooks(:,:,VarCounter)=double(Users{k}.Logbook(TimeInd+TD.User:TimeInd+TD.User-1+ControlPeriodsIt,[1,2,4:7,9]));
+    Logbooks1(:,1,VarCounter)=double(Users{k}.Logbook(TimeInd+TD.User:TimeInd+TD.User-1+ControlPeriodsIt,1));
+    Logbooks2(:,1,VarCounter)=double(Users{k}.Logbook(TimeInd+TD.User:TimeInd+TD.User-1+ControlPeriodsIt,2));
 end
 
 tic
 Availability1=zeros(ControlPeriodsIt,1,NumUsers);
 parfor VarCounter=1:NumUsers
-    Availability1(:,1,VarCounter)=max(0, ismember(Logbooks(:,1,VarCounter), 3:5) - Logbooks(:,2,VarCounter)/Time.StepMin) .* GCCA(:,VarCounter);
+    Availability1(:,1,VarCounter)=max(0, ismember(Logbooks1(:,1,VarCounter), 3:5) - Logbooks2(:,1,VarCounter)/Time.StepMin) .* GCCA(:,VarCounter);
 end
 toc
